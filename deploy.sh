40a1ec6688d8ec9e36b7df70352cb111580253d8
TrumpZeppeli <63998278+TrumpZeppeli@users.noreply.github.com>
2024-02-15 01:05:53 +0800

    [fix]:world

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:TwinZagreus/TwinZagreus.github.io.git master
-
+git push -f git@github.com:TwinZagreus/TwinZagreus.github.io.git main:gh-pages
+                
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
